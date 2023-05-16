0c0cb427566ffce78c0a7696f6a5e1ea951b9c2d
FadySabry712 <110053540+FadySabry712@users.noreply.github.com>
2023-05-17 01:49:40 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- Footer-->
+<footer>
+    <h5 id="footer-contnet">Scandiweb test Assigment</h5>
+</footer>   
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <title>Scandiweb Assigment</title>
+</head>
+
+<body>
+
+<!-- NAV Bar-->
+<nav class="navbar">
+    <div class="container">
+
+    
